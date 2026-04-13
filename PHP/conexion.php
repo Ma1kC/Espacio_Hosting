2aa86d1933bf186f7328e2a122fc69c864779a90
Maikk <mairachocou@gmail.com>
2026-04-13 13:39:46 -0300

    Create conexion.php

---
+++ PHP/conexion.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$usuario = "root";
+$password = "";
+$base_datos = "espacio hosting";
+
+$conn = new mysqli($host, $usuario, $password, $base_datos);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+?>
